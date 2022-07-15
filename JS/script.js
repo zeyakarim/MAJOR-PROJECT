6f5f1695322b8810a104ea51bb38a5ad235a2006
zeya karim <zeyakarim470@gmail.com>
2022-07-15 12:07:30 +0530

    Add Script file

---
+++ JS/script.js
@@ -0,0 +1,62 @@
+
+let ClickButton = function(btn){
+    $(btn).click(function(button){
+        let toggles = $(button.currentTarget).attr('data-toggles');
+        if(toggles == 'false'){
+            $(button.currentTarget).find('.music-menu-boxes').css('display','block');
+            $(button.currentTarget).attr('data-toggles', true);
+        }else{
+            $(button.currentTarget).find('.music-menu-boxes').css('display','none');
+            $(button.currentTarget).attr('data-toggles', false);
+        }
+    })
+}
+
+let clickImg = function(img){
+    $(img).click(function(imgclikced){
+        let imgtoggles = $(imgclikced.currentTarget).attr('data-toggles');
+
+        if (imgtoggles == 'false'){
+            $(imgclikced.currentTarget).find('.play-button1').css('display','block');
+            $(imgclikced.currentTarget).attr('data-toggles',true);
+        }else{
+            $(imgclikced.currentTarget).find('.play-button1').css('display','none');
+            $(imgclikced.currentTarget).attr('data-toggles',false);
+        }
+    })
+}
+
+let playbtn = function(menu){
+    $(menu).click(function(song){
+        // console.log($(song.currentTarget).attr('data-toggles'));
+        let menuClicked = $(song.currentTarget).attr('data-toggles');
+        if (menuClicked == 'false'){
+            $('#music-play-container').css('display','block');
+            $(song.currentTarget).attr('data-toggles',true);
+        }else{
+            $('#music-play-container').css('display','none');
+            $(song.currentTarget).attr('data-toggles',false);
+        }
+
+    })
+}
+
+let playbutton = $('.play-button1');
+
+for(i of playbutton){
+    playbtn(i);
+}
+
+
+let time = $('.time1');
+
+for(i of time){
+    ClickButton(i);
+}
+
+
+let imgHover = $('.image-over');
+
+for(img of imgHover){
+    clickImg(img);
+}
